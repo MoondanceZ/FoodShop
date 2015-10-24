7beb48ce786cad21e802765a7b8efaea8325e943
MoondanceZ <QYK_zZ@hotmail.com>
2015-10-24 12:24:55 +0800

    注册页面完成

---
+++ BLL/UserManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class UserManager
+    {
+        /// <summary>
+        /// 添加用户
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public static bool AddUser(MODEL.User user)
+        {
+            return DAL.UserServices.AddUser(user);
+        }
+    }
+}
--- FoodShop/Member/Register.aspx.cs
+++ FoodShop/Member/Register.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,9 +10,27 @@ namespace FoodShop.Member
 {
     public partial class Register : System.Web.UI.Page
     {
+        public bool login;
         protected void Page_Load(object sender, EventArgs e)
         {
-            
-        }        
+            if (IsPostBack)
+            {
+                MODEL.User user = new MODEL.User();
+                user.LoginId = Request.Form["txtName"].ToString();
+                user.PassWord = Request.Form["txtPwd"].ToString();
+                user.Guid = System.Guid.NewGuid().ToString();
+                user.Telephone = Request.Form["txtTel"].ToString();
+                user.Email = Request.Form["txtEmail"].ToString();
+                user.RegTime = DateTime.Now;
+                MODEL.UType uType = new MODEL.UType(1);                
+                user.UType = uType;
+                login = BLL.UserManager.AddUser(user);
+                if (login)
+                {
+                    //Thread.Sleep(5000);
+                    Response.Redirect("Index.aspx");
+                }
+            }
+        }
     }
 }
